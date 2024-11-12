56f976fc6884f1714a8b221da0765b9492431c68
Bhadreshkumar Ghevariya <bhadreshkumarghevariya@gmail.com>
2024-11-12 02:26:07 -0500

    completed tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold underline ">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
